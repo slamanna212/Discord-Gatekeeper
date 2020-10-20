a7a1b4c80aa2b5f94ac83c9a0f039bf35d746d80
Sam <slamanna212@gmail.com>
2020-10-19 21:12:50 -0400

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 # Bash file to set env vars in settings.json and start the bot
 
+# Make sure we are in correct directory
+cd /app
+
+# Use sed to add token env
+sed -i "s/\"token\": \".*\"/\"token\": \"$TOKEN\"/" settings.json
+
+# Use sed to add role env
+sed  -i "s/\"role\": .*,/\"role\": $ROLE,/" settings.json
+
+#Use sed to add channel env
+sed  -i "s/\"channel\": .*,/\"channel\": $CHANNEL,/" settings.json
+
+#Run the bot
+python /app/bot.py
